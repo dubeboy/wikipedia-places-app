cfefe30729a1ee906dbf49d0e73fb1f9bebca996
Divine Dube <divine@backbase.com>
2026-01-28 12:39:42 +0100

    Initial Commit

---
+++ Wikipedia Places App/Wikipedia_Places_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wikipedia_Places_AppApp.swift
+//  Wikipedia Places App
+//
+//  Created by Divine Dube on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Wikipedia_Places_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
